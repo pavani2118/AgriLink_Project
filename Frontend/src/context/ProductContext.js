e8c6ad0fbaf62f433ef5a1482beeae97abc847a3
Ridmi0826 <ridmisamarasundara351@gmail.com>
2026-02-09 18:35:57 +0530

    update codes

--- Frontend/src/context/ProductContext.js
+++ Frontend/src/context/ProductContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+ import React, { createContext, useEffect, useState } from "react";
 import { getAllProducts } from "../services/products";
 
 export const ProductContext = createContext();
